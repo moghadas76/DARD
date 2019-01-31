3141494abe83f56c0e9abb44a75fdeb8cd99ec20
Your Name <you@example.com>
2019-01-31 03:12:48 -0800

    Improve some bugs

--- app.js
+++ app.js
@@ -38,6 +38,19 @@ app.use(function(req, res, next) {
   next(createError(404));
 });
 
+app.use(function(req, res, next) {
+  var allowedOrigins = ['http://127.0.0.1:3000'];
+  var origin = req.headers.origin;
+  if(allowedOrigins.indexOf(origin) > -1){
+       res.setHeader('Access-Control-Allow-Origin', origin);
+  }
+  //res.header('Access-Control-Allow-Origin', 'http://127.0.0.1:8020');
+  res.header('Access-Control-Allow-Methods', 'GET, OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  res.header('Access-Control-Allow-Credentials', true);
+  return next();
+});
+
 // error handler
 app.use(function(err, req, res, next) {
   // set locals, only providing error in development
--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var mysql = require('mysql');
 var db = require('../config/mysql');
+var moment = require('moment');
 
 
 var router = express.Router();
@@ -39,7 +40,8 @@ router.post('/filter', function(req, res, next) {
 	var from = req.body.from;
 	var to = req.body.to;
 	var impi = req.body.impi;
-	var event_type = req.body.event_type;
+	var follow_type = req.body.follow_type;
+	console.log(follow_type);
 	var form_data = {start_date: req.body.start_date, start_time: req.body.start_time, stop_date : req.body.stop_date, stop_time:req.body.stop_time, from : from, to : to,impi : impi}
 	var query = "SELECT SIPCallsEvent.From,SIPCallsEvent.DSId,SIPCallsEvent.StatusStr,SIPCallsEvent.To,SIPCallsEvent.CallId,SIPCallsEvent.IMPI,SIPCallsEvent.FollowId,SIPCallsEvent.FollowTypeId,SIPCallsEvent.FollowTypeStr,SIPCallsEvent.StatusCode,SIPCallsEvent.StopTime,SIPCallsEvent.StartTime,SIPCallsEvent.UEIP,SIPCallsEvent.UEPort,SIPCallsEvent.UEAgent,SIPCallsEvent.CreateTime,UNIX_TIMESTAMP(SIPCallsEvent.CreateTime) as utime FROM "+db.TABLE+" WHERE 1=1 AND StartTime between '"+start_date+"' AND '"+stop_date+"' ";
 	if (from){
@@ -52,6 +54,27 @@ router.post('/filter', function(req, res, next) {
 		query += "AND "+db.TABLE+".IMPI LIKE '%"+impi+"%' ";	
 	}
 	
+	if (follow_type=="46"){
+		query += "AND "+db.TABLE+".FollowTypeId=46 ";
+	}
+	else if (follow_type=="1"){
+		query += "AND "+db.TABLE+".FollowTypeId=1 ";
+	}
+	else if (follow_type=="2"){
+		query += "AND "+db.TABLE+".FollowTypeId=2 ";
+	}
+	else if (follow_type=="3"){
+		query += "AND "+db.TABLE+".FollowTypeId=3 ";
+	}
+	else if (follow_type=="4"){
+		query += "AND "+db.TABLE+".FollowTypeId=4 ";
+	}
+	else if (follow_type=="47"){
+		query += "AND "+db.TABLE+".FollowTypeId=47 ";
+	}
+	else if (follow_type=="64"){
+		query += "AND "+db.TABLE+".FollowTypeId=64 ";
+	}
 	var EOQ = " ORDER BY StartTime DESC LIMIT "+PER_PAGE+" OFFSET "+skip +";";
 	var AOQ = " ORDER BY StartTime DESC ;";
 	var agr = query + AOQ;
@@ -82,6 +105,14 @@ router.post('/filter', function(req, res, next) {
 });
 
 
+router.get('/getCurrentTime',function(req, res, next){
+	moment.locale();
+	var cur_time = moment().format('HH:mm:ss');
+	console.log(cur_time);
+	res.json({current_time:cur_time});
+})
+
+
 router.get('/search', function(req, res, next) {
 	pageNum = req.query.page;
 	PER_PAGE = Number(req.query.per_page);
@@ -91,7 +122,7 @@ router.get('/search', function(req, res, next) {
 	var from = req.query.from;
 	var to = req.query.to;
 	var impi = req.query.impi;
-	var event_type = req.query.event_type;
+	var follow_type = req.query.follow_type;
 	var form_data = {start_date: req.query.start_date, start_time: req.query.start_time, stop_date : req.query.stop_date, stop_time:req.query.stop_time, from : from, to : to,impi : impi}
 	var query = "SELECT SIPCallsEvent.From,SIPCallsEvent.DSId,SIPCallsEvent.StatusStr,SIPCallsEvent.To,SIPCallsEvent.CallId,SIPCallsEvent.IMPI,SIPCallsEvent.FollowId,SIPCallsEvent.FollowTypeId,SIPCallsEvent.FollowTypeStr,SIPCallsEvent.StatusCode,SIPCallsEvent.StopTime,SIPCallsEvent.StartTime,SIPCallsEvent.UEIP,SIPCallsEvent.UEPort,SIPCallsEvent.UEAgent,SIPCallsEvent.CreateTime,UNIX_TIMESTAMP(SIPCallsEvent.CreateTime) as utime FROM "+db.TABLE+" WHERE 1=1 AND StartTime between '"+start_date+"' AND '"+stop_date+"' ";
 	if (from){
@@ -103,7 +134,27 @@ router.get('/search', function(req, res, next) {
 	if (impi){
 		query += "AND "+db.TABLE+".IMPI LIKE '%"+impi+"%' ";	
 	}
-	
+	if (follow_type=="46"){
+		query += "AND "+db.TABLE+".FollowTypeId=46 ";
+	}
+	else if (follow_type=="1"){
+		query += "AND "+db.TABLE+".FollowTypeId=1 ";
+	}
+	else if (follow_type=="2"){
+		query += "AND "+db.TABLE+".FollowTypeId=2 ";
+	}
+	else if (follow_type=="3"){
+		query += "AND "+db.TABLE+".FollowTypeId=3 ";
+	}
+	else if (follow_type=="4"){
+		query += "AND "+db.TABLE+".FollowTypeId=4 ";
+	}
+	else if (follow_type=="47"){
+		query += "AND "+db.TABLE+".FollowTypeId=47 ";
+	}
+	else if (follow_type=="64"){
+		query += "AND "+db.TABLE+".FollowTypeId=64 ";
+	}
 	var EOQ = " ORDER BY StartTime DESC LIMIT "+PER_PAGE+" OFFSET "+skip +";";
 	var AOQ = " ORDER BY StartTime DESC ;";
 	var agr = query + AOQ;
